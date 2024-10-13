4b61f50e2969e61e49a457a23178ba34477e12e9
Adam-Golan <slowmoon.adam@gmail.com>
2024-10-13 08:56:57 +0300

    Updating capitalize method -> avoiding redundant loops and using efficient native code.

--- src/utils/stringExtensions.ts
+++ src/utils/stringExtensions.ts
@@ -1,16 +1,14 @@
 String.prototype.capitalize = function (): string {
-     // Convert the string to an array of characters for efficient manipulation
-     const chars = this.toLowerCase().split('');
-     // Capitalize the first character
-     chars[0] = chars[0].toUpperCase();
- 
-     // Iterate through the characters and capitalize after each space
-     for (let i = 1; i < chars.length; i++) {
-         if (chars[i - 1] === ' ') {
-             chars[i] = chars[i].toUpperCase();
-         }
-     }
- 
-     // Join the characters back into a string
-     return chars.join('');
+    // Convert the string to an array of characters for efficient manipulation
+    const chars = this.toLowerCase().trim().split('');
+    // Capitalize the first character
+    chars[0] = chars[0].toUpperCase();
+
+    // Iterate through the characters and capitalize after each space
+    if (chars.includes(' '))
+        for (const [i, _] of chars.entries())
+            if (chars[i - 1] === ' ') chars[i] = chars[i].toUpperCase();
+
+    // Join the characters back into a string
+    return chars.join('');
 };
