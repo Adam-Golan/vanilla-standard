e910eff2e0a0748b46d5fca16ae6b222666b4ac4
Adam-Golan <slowmoon.adam@gmail.com>
2024-11-17 13:38:13 +0200

    Avatar style done.

--- src/app/shared/native/avatar/avatar.ts
+++ src/app/shared/native/avatar/avatar.ts
@@ -1,12 +1,14 @@
 import { ComponentDecorator } from "@decorators";
 
 import './avatar.scss';
+import { addMeta } from "@decorators/utils/adders";
 
 @ComponentDecorator
 export class Avatar extends HTMLElement {
 
     constructor(url: string, width: number = 100) {
         super();
+        addMeta(this, 'component');
         this.setAttribute('role', 'img');
         this.setAttribute('alt', 'avatar');
         this.style.width = `${width}px`;
