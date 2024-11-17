358d0bedfa0334e7cbd883632c310477c152122b
Adam-Golan <slowmoon.adam@gmail.com>
2024-11-17 08:54:16 +0200

    Adding meta to Tester.

--- src/app/shared/native/tester/tester.ts
+++ src/app/shared/native/tester/tester.ts
@@ -1,6 +1,12 @@
 import { ComponentDecorator } from "@decorators";
+import { addMeta } from "@decorators/utils/adders";
 
 import './tester.scss';
 
 @ComponentDecorator
-export class Tester extends HTMLElement {}
+export class Tester extends HTMLElement {
+    constructor() {
+        super();
+        addMeta(this, 'component');
+    }
+}
