118776ff6e094c9d09dd1834898aa35473f3900b
Adam-Golan <slowmoon.adam@gmail.com>
2024-11-22 18:03:56 +0200

    Realigning to implement.

---
+++ src/i18n/en/documentation/documentation.ts
@@ -0,0 +1,11 @@
+import { FooterText } from "@app/shared/data/link-based/intefaces";
+import { ISoonText } from "@app/shared/text/soon/lang";
+
+export const docs = {
+    SOON: {
+        pageName: 'documentation'
+    } as ISoonText,
+    FOOTER: {
+        RIGHTS: 'rights'
+    } as FooterText
+}
--- src/i18n/en/lang.ts
+++ src/i18n/en/lang.ts
@@ -1,9 +1,15 @@
 import { home } from './home/home';
 import { contactus } from './contact/contact';
+import { forms } from './forms/forms';
+import { dialogs } from './dialogs/dialogs';
+import { docs } from './documentation/documentation';
 import { getstarted } from './get-started/get-started';
 
 export const texts = {
     home,
+    forms,
+    dialogs,
+    docs,
     getstarted,
     contactus
 };
