3fc0b3da3fe31c791f3448ef5014ca13d7868cbc
Adam-Golan <slowmoon.adam@gmail.com>
2024-11-22 17:54:46 +0200

    Refactoring language service into dev choice -> direct/dynamic import.

--- src/services/language/language.ts
+++ src/services/language/language.ts
@@ -1,9 +1,22 @@
-import { texts } from '../../i18n/en/lang';
+// Activate if no need in dynamic import.
+// import { texts } from '../../i18n/en/lang';
 
 export class Language {
-    texts = texts;
+    texts: any;
 
-    getPageTexts(page: keyof typeof texts): any {
-        return this.texts[page];
+    getTexts(path: string): any {
+        return path.includes('/') ? this.locateTexts(path.split('/')) : this.texts[path];
+    }
+
+    private locateTexts(paths: string[]): any {
+        let ref = this.texts;
+        for (const subPath of paths) ref = ref[subPath];
+        return ref;
+    }
+
+    async importTexts(lang: string, subPath: string = ''): Promise<void> {
+        // Deactivate if no need in dynamic import.
+        const { texts } = await import(/* @vite-ignore */`../../i18n/${lang}/lang${subPath}`);
+        this.texts = texts;
     }
 }
