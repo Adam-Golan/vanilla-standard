b796b635300c0759de4d3c7098df209f3ddc2fcf
Adam-Golan <slowmoon.adam@gmail.com>
2024-11-22 18:01:59 +0200

    Refactoring main -> implementing refactored navigation, erasing interface, cutting unwanted code.

--- src/main.ts
+++ src/main.ts
@@ -6,30 +6,22 @@ import { Modal, Navbar } from "@app/shared";
 import { StateKeys } from '@constants/stateKeys.constant';
 import { appConfig } from 'app.config';
 
-interface IApplicationState {
-  [StateKeys.lang]: Language;
-  [StateKeys.device]: Device;
-}
-
 class Main {
   // App element.
   app = document.getElementById('app') ?? this.createApp();
   // Services.
   device: Device;
-  navigator: Navigation;
-  appState: State<IApplicationState>;
+  navigation: Navigation;
+  appState: State;
   i18n: Language;
 
   // Elements.
   constructor() {
     setMetaTags(appConfig.meta);
     if (appConfig.OGCard) setOpenGraphTags(appConfig.OGCard);
-    this.device = new Device();
     this.appState = new State();
-    this.i18n = new Language();
-    this.navigator = new Navigation(this.appState, this.app, appConfig.routes);
-    this.setData();
-    this.init();
+    this.navigation = new Navigation(this.appState, this.app, appConfig.routes);
+    this.navigation.importTexts().then(_ => this.init()); // Importing application's texts.
   }
 
   private createApp(): HTMLDivElement {
@@ -39,27 +31,18 @@ class Main {
     return app;
   }
 
-  private setData(): void {
-    this.appState.setData(StateKeys.lang, this.i18n);
-    this.appState.setData(StateKeys.device, this.device);
-  }
-
   private init() {
-    this.app.append(new Navbar(this.navigator.pages, this.appState));
+    this.app.append(new Navbar(this.navigation.pages, this.appState));
     this.subscribes();
-    this.navigator.fisrtLoad(location.pathname);
-  }
-
-  private navigation(page: string): void {
-    this.navigator.loading(page);
+    this.navigation.fisrtLoad(location.pathname);
   }
 
   private subscribes(): void {
     const modals: { [key: string]: Modal } = {};
     // Page Navigation.
-    this.appState.subscribe(StateKeys.stateNavigate, this.navigation.bind(this));
+    this.appState.subscribe(StateKeys.navigate, (page) => this.navigation.loading(page));
     // Load Page.
-    this.appState.subscribe(StateKeys.pageContentLoaded, _ => this.navigator.showPage());
+    this.appState.subscribe(StateKeys.contentReady, _ => this.navigation.showPage());
     // Modals.
     this.appState.subscribe(StateKeys.openModal, (key: string, content) => modals[key] = new Modal(this.app.append, content));
     this.appState.subscribe(StateKeys.closeModal, (key: string) => { modals[key].closeModal(); delete modals[key]; });
