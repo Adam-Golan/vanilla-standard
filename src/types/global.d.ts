5e1903c394de8c989c9f48345aadbf30fad74c8d
Adam-Golan <slowmoon.adam@gmail.com>
2024-11-28 13:57:30 +0200

    Relocating utils declarations.

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,12 +1,3 @@
-declare global {
-    interface String {
-        capitalize(): string;
-        addSpaces(type: 'uppercase' | '-' | '_' | '/'): string;
-    }
-
-    interface Date {
-        getTimeDiff(b: Date): IDateTimeDiff
-    }
-}
+declare global {}
 
 export { };
