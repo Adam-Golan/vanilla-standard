9610bfc623d888a8fc02937e768ef838474192f5
Adam-Golan <slowmoon.adam@gmail.com>
2024-12-02 18:49:56 +0200

    Component -> Adding optional parentState and adding JSDoc.

--- src/decorators/component/component.ts
+++ src/decorators/component/component.ts
@@ -2,13 +2,24 @@ import { Enlist, addMeta } from "@decorators/utils";
 import { Basis } from "../base";
 import { State } from "@services";
 
-// Enlisting component into custom elements registry.
-export function ComponentDecorator(target: CustomElementConstructor) {
+/**
+ * Enlists a component into custom elements registry.
+ *
+ * @param target - A constructor function for a custom element.
+ * @returns The same constructor function, but with the component enlisting decorator.
+ */
+export function ComponentDecorator<T extends CustomElementConstructor>(target: T): void {
     Enlist('component', target);
 }
 
 export abstract class Component<IConfig = Record<string, any>> extends Basis<IConfig> {
-    constructor(protected config: IConfig, protected moduleState?: State) {
+    /**
+     * Constructs a Component instance.
+     *
+     * @param config - Configuration object of type IConfig.
+     * @param parentState - Optional parent state.
+     */
+    constructor(protected config: IConfig, protected parentState?: State) {
         super(config);
         addMeta(this, 'component');
     }
