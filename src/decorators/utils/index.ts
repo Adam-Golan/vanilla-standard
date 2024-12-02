83320c9903bdf7a5fde982c088d056c258bc67be
Adam-Golan <slowmoon.adam@gmail.com>
2024-12-02 18:46:56 +0200

    Utils -> Implementing new type names and adding JSDoc.

--- src/decorators/utils/index.ts
+++ src/decorators/utils/index.ts
@@ -1,13 +1,26 @@
-import { ElementType } from "../types/types";
+import { BlockType } from "../types";
 
-export function Enlist(type: ElementType, target: CustomElementConstructor) {
+/**
+ * Enlists a custom element into Custom Elements Registry.
+ *
+ * @param type - a type of custom element (page, module, component)
+ * @param target - a custom element constructor
+ */
+export function Enlist(type: BlockType, target: CustomElementConstructor) {
     customElements.define(`${target.name.toLowerCase()}-${type}`, target);
 }
 
-export function addMeta(target: HTMLElement, cls: 'page' | 'module' | 'component') {
+/**
+ * Adds meta information to a custom element.
+ *
+ * @param target - a custom element
+ * @param type - a type of custom element (page, module, component)
+ */
+
+export function addMeta(target: HTMLElement, type: BlockType) {
     const name = target.constructor.name.toLowerCase();
     // Create id for pages or class for modules and components by constructor name.
-    target[cls === 'page' ? 'id' : 'className'] = name;
+    target[type === 'page' ? 'id' : 'className'] = name;
     // Setting data-type.
-    target.dataset.type = cls;
+    target.dataset.type = type;
 }
