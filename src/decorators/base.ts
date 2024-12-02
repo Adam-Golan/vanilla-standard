00f58b8d8550771e185fd120c980683e4be1875e
Adam-Golan <slowmoon.adam@gmail.com>
2024-12-02 18:44:49 +0200

    Basis -> Adding JSDoc and reverting init into it's original approach.

--- src/decorators/base.ts
+++ src/decorators/base.ts
@@ -1,37 +1,62 @@
 export abstract class Basis<IConfig> extends HTMLElement {
+    
+    /**
+     * Construct a new `Basis` instance.
+     *
+     * @param config The configuration object for this component.
+     *
+     * The `init` method is called automatically after a short delay to allow the
+     * component to be fully initialized before the `init` method is called.
+     */
     constructor(protected config: IConfig) {
         super();
-        this.callInit();
-    }
-    
-    private callInit(): void {
-        Promise.resolve().then(() => this.init());
+        setTimeout(this.init.bind(this));
     }
 
     protected abstract init(): void;
 
-    // Create element.
+    /**
+     * Create an element with a given tag name.
+     * @param tag The tag name of the element to create, e.g. "div", "span", etc.
+     * @returns The created element.
+     */
     protected cElem<K extends keyof HTMLElementTagNameMap>(tag: K): HTMLElementTagNameMap[K] {
-        return document.createElement(tag);
+        return document.createElement<K>(tag);
     }
 
-    // Get element -> Id.
-    protected idElem(id: string): HTMLElement | null {
+    /**
+     * Get an element by its id.
+     * @param id The id of the element to retrieve.
+     * @returns The element with the given id, or null if it doesn't exist.
+     */
+    protected idElem<K extends keyof HTMLElementTagNameMap>(id: K): HTMLElementTagNameMap[K] | null {
         return this.querySelector(`#${id}`);
     }
 
-    // Get element -> Name.
-    protected nmElem(name: string): NodeListOf<HTMLElement> {
+    /**
+     * Get elements by their "name" attribute.
+     * @param name The value of the "name" attribute to search for.
+     * @returns A list of elements with a "name" attribute equal to the given value.
+     */
+    protected nmElem<K extends string>(name: K): NodeListOf<HTMLInputElement> {
         return this.querySelectorAll(`[name="${name}"]`);
     }
 
-    // Get element -> Local Class.
-    protected clsElem(className: string): NodeListOf<HTMLElement> {
+    /**
+     * Get elements by their "class" attribute.
+     * @param className The value of the "class" attribute to search for.
+     * @returns A list of elements with a "class" attribute equal to the given value.
+     */
+    protected clsElem<K extends string>(className: K): NodeListOf<HTMLElement & { className: K }> {
         return this.querySelectorAll(`.${className}`);
     }
 
-    // Get element -> Local Tag.
+    /**
+     * Get elements by their tag name.
+     * @param tag The tag name of the elements to retrieve, e.g. "div", "span", etc.
+     * @returns A collection of elements with the specified tag name.
+     */
     protected tagElem<K extends keyof HTMLElementTagNameMap>(tag: K): HTMLCollectionOf<HTMLElementTagNameMap[K]> {
-        return this.getElementsByTagName(tag);
+        return this.getElementsByTagName<K>(tag);
     }
 }
