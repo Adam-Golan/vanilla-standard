f5bf13a2724e9443fb9ba5bfc55619be05bc8a83
Adam-Golan <slowmoon.adam@gmail.com>
2024-12-02 18:51:35 +0200

    Module -> Adding JSDoc.

--- src/decorators/module/module.ts
+++ src/decorators/module/module.ts
@@ -3,15 +3,25 @@ import { Enlist, addMeta } from "@decorators/utils";
 import { State } from "@services";
 
 
-// Enlisting module into custom elements registry.
-export function ModuleDecorator(target: CustomElementConstructor) {
+/**
+ * Enlists a custom element into Custom Elements Registry as a module.
+ *
+ * @param target - a custom element constructor
+ */
+export function ModuleDecorator<T extends CustomElementConstructor>(target: T): void {
     Enlist('module', target);
 }
 
 export abstract class Module<IConfig = Record<string, any>> extends Basis<IConfig> {
     state = new State();
 
-    constructor(protected config: IConfig, protected pageState?: State) {
+    /**
+     * Constructs a Module instance.
+     *
+     * @param config - Configuration object of type IConfig.
+     * @param parentState - Optional parent state.
+     */
+    constructor(protected config: IConfig, protected parentState?: State) {
         super(config);
         addMeta(this, 'module');
     }
