ebba442fc39fcb96c32ac5bdda5709c4a05a0a4d
Adam-Golan <slowmoon.adam@gmail.com>
2024-12-05 15:19:12 +0200

    AppConfig -> Implementing Navigation new rules.

--- src/app.config.ts
+++ src/app.config.ts
@@ -6,11 +6,11 @@ import type { IPage, IPages } from "@services/navigation/types";
 export const appConfig: AppConfig = {
     siteURL: 'https://your-site.com', // Replace with your site's actual URL
     routes: new Map<string, IPage>([
-        ['home', Home],
-        ['get-started', GetStarted],
-        ['docs', Documentation],
-        ['about-us', AboutUs],
-        ['contact-us', ContactUs],
+        ['/home', Home],
+        ['/get-started', GetStarted],
+        ['/docs', Documentation],
+        ['/about-us', AboutUs],
+        ['/contact-us', ContactUs],
     ]),
     meta: {
         description: "Welcome to Vanilla, a fast and reliable web development frame.",
