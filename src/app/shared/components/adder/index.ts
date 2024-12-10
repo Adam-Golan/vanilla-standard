da32769790ca48117c8b25a1b6ae185d510902cf
Adam-Golan <slowmoon.adam@gmail.com>
2024-12-10 10:07:42 +0200

    Adder -> Fix modalKey bug.

--- src/app/shared/components/adder/index.ts
+++ src/app/shared/components/adder/index.ts
@@ -7,13 +7,13 @@ import './adder.scss';
 
 @ComponentDecorator
 export class Adder extends HTMLElement {
-    constructor(protected appState: State, private comp: HTMLElement) {
+    constructor(protected appState: State, private modalKey: string, private comp: HTMLElement) {
         super();
         this.init();
         addMeta(this, 'component');
     }
     protected init(): void {
         this.id = 'adder';
-        this.onclick = () => this.appState.publish(StateKeys.openModal, this.comp);
+        this.onclick = () => this.appState.publish(StateKeys.openModal, this.modalKey, this.comp);
     }
 }
